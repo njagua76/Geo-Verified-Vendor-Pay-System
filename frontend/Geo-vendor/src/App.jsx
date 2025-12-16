12cf41a20a1b260e9d162a21e4534d27532f185f
Ann Gathoni <theanngathoni@gmail.com>
2025-12-16 14:42:53 +0300

    login page

--- frontend/Geo-vendor/src/App.jsx
+++ frontend/Geo-vendor/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<h1>Admin Dashboard</h1>} />
-        <Route path="/verify" element={<h1>Field Agent Page</h1>} />
+        
+        
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
